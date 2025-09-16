82b2867ac4230bee65f07c1b2a84be4ea4264177
onoe yoshiyuki <onoe.sanpo@gmail.com>
2025-09-16 22:04:29 +0900

    会員登録ページ、ログインページスタイル調整完了

---
+++ src/resources/views/layouts/app_auth.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Coachtech-Fleamarket</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header-utilities">
+                <a class="header__logo" href="/">
+                    <img src="{{ asset('images/logo.svg') }}" alt="Logo" class="header__logo-image">
+                </a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+@yield('js')
+</body>
+
+</html>
